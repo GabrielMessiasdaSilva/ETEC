8ab11355b25b46f9c9987dd3b93341111dca1413
FATEC ZONA LESTE <gabrielestudante420@gmail.com>
2023-03-13 13:56:21 -0300

    adicionando mais arquivos

---
+++ BD1/exercicios/Exercicio1.sql
@@ -0,0 +1,21 @@
+--~Comando para criação de um banco de dados 
+CREATE DATABASE ExercBD01
+
+use ExercBD01
+--Criando tabela 
+CREATE TABLE pet
+(
+NumRegistro int,
+Nome varchar(80),
+Especie varchar(25),
+Raca varchar(30),
+Cor varchar(40),
+nascimento datetime,
+Sexo varchar (9)
+)
+
+
+
+
+
+
